059d76580b9c5c9f6857e5b433e59967d595ec00
Alexey Grigorenko <117086712+Eversus@users.noreply.github.com>
2023-03-06 21:40:56 +0300

    Added 2 modifiers

---
+++ admin/config.php
@@ -0,0 +1,34 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'https://octest.web-space.su/admin/');
+define('HTTP_CATALOG', 'https://octest.web-space.su/');
+
+// HTTPS
+define('HTTPS_SERVER', 'https://octest.web-space.su/admin/');
+define('HTTPS_CATALOG', 'https://octest.web-space.su/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/octest/data/www/octest.web-space.su/admin/');
+define('DIR_SYSTEM', '/var/www/octest/data/www/octest.web-space.su/system/');
+define('DIR_IMAGE', '/var/www/octest/data/www/octest.web-space.su/image/');
+define('DIR_LANGUAGE', '/var/www/octest/data/www/octest.web-space.su/admin/language/');
+define('DIR_TEMPLATE', '/var/www/octest/data/www/octest.web-space.su/admin/view/template/');
+define('DIR_CONFIG', '/var/www/octest/data/www/octest.web-space.su/system/config/');
+define('DIR_CACHE', '/var/www/octest/data/www/octest.web-space.su/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/octest/data/www/octest.web-space.su/system/storage/download/');
+define('DIR_LOGS', '/var/www/octest/data/www/octest.web-space.su/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/octest/data/www/octest.web-space.su/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/octest/data/www/octest.web-space.su/system/storage/upload/');
+define('DIR_CATALOG', '/var/www/octest/data/www/octest.web-space.su/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'octestuser');
+define('DB_PASSWORD', 'aG2hH3eU9o');
+define('DB_DATABASE', 'octestbase');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCartForum API
+define('OPENCARTFORUM_SERVER', 'https://opencartforum.com/');
---
+++ config.php
@@ -0,0 +1,33 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'https://octest.web-space.su/');
+
+// HTTPS
+define('HTTPS_SERVER', 'https://octest.web-space.su/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/octest/data/www/octest.web-space.su/catalog/');
+define('DIR_SYSTEM', '/var/www/octest/data/www/octest.web-space.su/system/');
+define('DIR_IMAGE', '/var/www/octest/data/www/octest.web-space.su/image/');
+define('DIR_LANGUAGE', '/var/www/octest/data/www/octest.web-space.su/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/octest/data/www/octest.web-space.su/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/octest/data/www/octest.web-space.su/system/config/');
+define('DIR_CACHE', '/var/www/octest/data/www/octest.web-space.su/system/storage/cache/');
+define('DIR_DOWNLOAD', '/var/www/octest/data/www/octest.web-space.su/system/storage/download/');
+define('DIR_LOGS', '/var/www/octest/data/www/octest.web-space.su/system/storage/logs/');
+define('DIR_MODIFICATION', '/var/www/octest/data/www/octest.web-space.su/system/storage/modification/');
+define('DIR_UPLOAD', '/var/www/octest/data/www/octest.web-space.su/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'octestuser');
+define('DB_PASSWORD', 'aG2hH3eU9o');
+define('DB_DATABASE', 'octestbase');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// Redis
+// define('CACHE_HOSTNAME', 'localhost');
+// define('CACHE_PORT', '6379');
+// define('CACHE_PREFIX', 'oc_');
